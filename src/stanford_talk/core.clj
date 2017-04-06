f04e1ea0c273872a1dd0333ce19c6019f99f6151
martin <martinobal@gmail.com>
2017-04-06 20:10:26 +0200

    remove with-err-str macro from core and parser

--- src/stanford_talk/core.clj
+++ src/stanford_talk/core.clj
@@ -3,16 +3,6 @@
   (:gen-class))
 
 
-(use '[clojure.repl :only (pst)])
-(defmacro with-err-str
-  "Evaluates exprs in a context in which *err* is bound to a fresh
-  StringWriter.  Returns the string created by any nested printing
-  calls."
-  [& body]
-  `(let [s# (new java.io.StringWriter)]
-     (binding [*err* s#]
-       ~@body
-       (str s#))))
 
 
 
--- src/stanford_talk/parser.clj
+++ src/stanford_talk/parser.clj
@@ -9,16 +9,7 @@
            
            
 
-(use '[clojure.repl :only (pst)])
-(defmacro with-err-str
-  "Evaluates exprs in a context in which *err* is bound to a fresh
-  StringWriter.  Returns the string created by any nested printing
-  calls."
-  [& body]
-  `(let [s# (new java.io.StringWriter)]
-     (binding [*err* s#]
-       ~@body
-       (str s#))))
+
 
 ;;(def stp-out (promise))
 ;;(def stp-err (promise))
